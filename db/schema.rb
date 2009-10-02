5083bff5f1f908aab808305eaf817f441a5ac541
Thomas Volkmar Worm <thomas@s4r.de>
2009-10-02 22:51:47 +0200

    added activerecord session store

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 4) do
+ActiveRecord::Schema.define(:version => 5) do
 
   create_table "folders", :force => true do |t|
     t.string   "name"
@@ -71,6 +71,16 @@ ActiveRecord::Schema.define(:version => 4) do
   add_index "myfiles", ["folder_id"], :name => "index_myfiles_on_folder_id"
   add_index "myfiles", ["indexed"], :name => "index_myfiles_on_indexed"
   add_index "myfiles", ["user_id"], :name => "index_myfiles_on_user_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "usages", :force => true do |t|
     t.datetime "download_date_time"
